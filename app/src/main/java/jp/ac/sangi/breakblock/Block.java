8e370c2d9e0118ea122da4bf1b9ed91ed8319cc5
r-suzuki <suzuki.rui@sist.ac.jp>
2018-09-20 10:27:08 +0900

    インデント修正

--- app/src/main/java/jp/ac/sangi/breakblock/Block.java
+++ app/src/main/java/jp/ac/sangi/breakblock/Block.java
@@ -12,9 +12,9 @@ public class Block {
     // 2018.09.19 R.Suzuki 新規作成
     */
     public Block(float top, float left, float bottom, float right) {
-        this.top = top;
-        this.left = left;
-        this.bottom = bottom;
-        this.right = right;
+        this.top	= top;
+        this.left	= left;
+        this.bottom	= bottom;
+        this.right	= right;
     }
 }
